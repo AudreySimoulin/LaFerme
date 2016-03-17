4308af5fa03a4072d416c46e537658b938182191
aaventin <agathe.aventin@soprasteria.com>
2016-03-17 12:30:02 +0100

    Affichage date + durée de vie ok

---
+++ src/main/java/LaFerme/servlet/NourrirServlet.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LaFerme.servlet;
+
+import LaFerme.spring.AutowireServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "NourrirServlet", urlPatterns = {"/nourrir_servlet"})
+public class NourrirServlet extends AutowireServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    
+
+
+}
